5a5752a641eb46081577aa17d0a15a0a721b7dcb
YonM <yonathanmaalo@gmail.com>
2016-12-24 11:08:33 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <Timer :time="10"></Timer>
+  </div>
+</template>
+
+<script>
+import Timer from './Timer.vue';
+export default {
+  name: 'app',
+  components: {
+    Timer
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/Timer.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <h1>{{value}} </h1>
+  <button @click="value-=1">Start Timer! </button>
+</div>
+</template>
+
+<script>
+var T= {
+  name: "Timer",
+  props: {
+    time: Number
+  }, 
+  data() {
+    return {
+      value: this.time
+    }
+  },
+  methods: {
+    countdown: () => {
+      setInterval(function() {
+        this.value = this.value - 1;
+        console.log("countdown initiated from: " + this.value + " " + this.time);
+      }, 1000);
+    }
+  }
+}  
+export default T;  
+  
+</script>
+
+<style></style>
