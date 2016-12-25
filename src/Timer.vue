b0cf48c649737a191f84d4bcc094d8eecdb956bb
faheem <faheem@faheempatel.com>
2016-12-25 03:25:19 +0000

    Implement start, stop and reset functionality

--- src/Timer.vue
+++ src/Timer.vue
@@ -1,7 +1,9 @@
 <template>
 <div>
   <h1>{{value}}</h1>
-  <button @click="countdown">Start Timer! </button>
+  <button @click="start">Start Timer!</button>
+  <button @click="stop">Stop</button>
+  <button @click="reset">Reset</button>
 </div>
 </template>
 
@@ -13,15 +15,23 @@ export default {
   }, 
   data() {
     return {
-      value: this.time
+      value: this.time,
+      timer: undefined,
     }
   },
   methods: {
-    countdown: function() {
-      const timer = setInterval(_ => {
+    start: function() {
+      this.timer = setInterval(_ => {
         this.value = this.value - 1;
-        if (this.value === 0) clearInterval(timer);
+        if (this.value === 0) clearInterval(this.timer);
       }, 1000);
+    },
+    stop: function() {
+      clearInterval(this.timer);
+    },
+    reset: function() {
+      this.stop();
+      this.value = this.time;
     }
   }
 }  
